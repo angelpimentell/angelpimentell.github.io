58f6fe2e0fa8b28cf012f922690c722cfc4e133d
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-14 14:09:43 -0400

    Change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-function main(){
+function main() {
     console.log("DEVICES:")
-    navigator.usb.getDevices().then((devices) => {
-        console.log(`Total devices: ${devices.length}`);
-        devices.forEach((device) => {
-          console.log(
-            `Product name: ${device.productName}, serial number ${device.serialNumber}`,
-          );
+    // Get all connected USB devices the website has been granted access to.
+    navigator.usb.getDevices().then(devices => {
+        devices.forEach(device => {
+            console.log(device.productName);      // "Arduino Micro"
+            console.log(device.manufacturerName); // "Arduino LLC"
         });
-      });
+    })
 }
