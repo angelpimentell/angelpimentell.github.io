c984ffe92e11c0fa3247fee307c3c83074171a79
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-14 13:54:41 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function main(){
+    console.log("DEVICES:")
+    navigator.usb.getDevices().then((devices) => {
+        console.log(`Total devices: ${devices.length}`);
+        devices.forEach((device) => {
+          console.log(
+            `Product name: ${device.productName}, serial number ${device.serialNumber}`,
+          );
+        });
+      });
+}
